79f2d857c59f59d68385b836a37d1389c4e31bc2
bzn <coxherbert98@gmail.com>
2026-08-27 08:39:17 +0800

    fix: 更新文件夹名称

--- ifs-portal/src/main.ts
+++ ifs-portal/src/main.ts
@@ -3,10 +3,10 @@ import Antd from 'ant-design-vue'
 import VXETable from 'vxe-table'
 import 'ant-design-vue/dist/reset.css'
 import 'vxe-table/lib/style.css'
-import router from './router'
+import router from './router/index.ts'
 import App from './App.vue'
 import './style.css'
-import { checkAppVersion } from './utils/version-check'
+import { checkAppVersion } from './utils/version-check.ts'
 
 createApp(App).use(router).use(Antd).use(VXETable).mount('#app')
 
